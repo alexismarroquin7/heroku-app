85024687179b8de1c1661d731c920fe8f433b83c
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-08 09:57:53 -0700

    initializes server

---
+++ index.js
@@ -0,0 +1,24 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", (_, res) => {
+  res.json({ data: "API is accounted for" });
+});
+
+app.listen(port, () => {
+  console.log(`\nListening on port ${port}\n`);
+});
+
+// console.log("it's alive");
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.env.USER);
+// console.log(process.env.PORT);
+// console.log(process.env.FOOD);
+// console.log(process.env.GREET);
