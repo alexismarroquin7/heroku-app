66bda76cdce931ba3009ff44e5d9c3dcb2f35180
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-08 10:52:07 -0700

    push

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use("/api", (_, res) => {
   res.json({ data: "API is accounted for" });
